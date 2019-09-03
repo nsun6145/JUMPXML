4bf342b3598fc622401835e03331eab92c4fff65
nsun6145 <njs6145@g.rit.edu>
2019-09-03 09:38:59 -0400

    replaced wrong files

---
+++ Part2/SAXEventHandler/src/com/example/sax/read/ReadXMLWithSAX.java
@@ -0,0 +1,16 @@
+ package com.example.sax.read;
+
+import com.example.dataprovider.DataProvider;
+
+public class ReadXMLWithSAX {
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws Exception {
+
+		String filename = DataProvider.DATADIR + "customers.xml";
+		
+		SAXCustomerHandler saxHandler = new SAXCustomerHandler();
+		saxHandler.readDataFromXML(filename);
+	}
+
+}
---
+++ Part2/SAXEventHandler/src/com/example/sax/read/SAXCustomerHandler.java
@@ -0,0 +1,61 @@
+package com.example.sax.read;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+@SuppressWarnings("unused") 
+public class SAXCustomerHandler extends DefaultHandler {
+
+	private List<Customer> data;
+	private static final String XMLDATEFORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+
+	public List<Customer> readDataFromXML(String filename) throws SAXException, IOException, ParserConfigurationException {
+		
+		
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		SAXParser parser = factory.newSAXParser();
+		
+		parser.parse(new File(filename), this);
+		
+		return data;
+	}
+	
+	@Override
+	public void startDocument() throws SAXException {
+		System.out.println("Start document");
+	}
+	
+	@Override
+	public void endDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		System.out.println("End document");
+	}
+	
+	@Override
+	public void startElement(String uri, String localName, 
+			String qName, Attributes attributes) throws SAXException {
+		System.out.println("Start element: " + qName);
+	}
+	
+	@Override
+	public void endElement(String uri, String localName, String qName)
+			throws SAXException {
+		System.out.println("End element: " + qName);
+		
+	}
+	
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		System.out.println("Characters");
+	}
+	
+}
